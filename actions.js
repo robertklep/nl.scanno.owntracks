1dd30eabf0071aa61950975758cb2b6759fa4ca4
Menno van Grinsven <scanno71@gmail.com>
2017-04-25 18:46:50 +0200

    Use Promises to make sure we get the address before we answer

--- actions.js
+++ actions.js
@@ -71,11 +71,18 @@ function registerSpeech() {
               foundUser = globalVar.getUserFromString(speech.transcript);
               if (foundUser !== null) {
                  logmodule.writelog("Found user: " + foundUser.userName + "   Fence: " + foundUser.fence);
-                 speech.say( getLocationString(foundUser.userName) );
-                 callback( null, true );
+                 return getLocationString(foundUser.userName).then(function(speechline) {
+                    logmodule.writelog("Speech output: " + speechline);
+                    speech.say( speechline );
+                    callback( null, true );
+                 });
               } else {
                  logmodule.writelog("No user found");
-                 speech.say( getLocationString(null) );
+                 return getLocationString(null).then(function(speechline) {
+                    logmodule.writelog("Speech output: " + speechline);
+                    speech.say( speechline );
+                    callback( null, true );
+                 });
               }
             }
        });
@@ -87,60 +94,83 @@ function registerSpeech() {
 }
 
 function getLocationString(userName) {
-   var locationString = '';
-   logmodule.writelog("Create LocationString for user: " + userName);
-   if ( userName !== null && globalVar.getUser(userName).fence !== null && globalVar.getUser(userName).fence !== undefined ) {
-      if ( globalVar.getUser(userName).fence !== "" ) {
-         locationString = __("location_known", {"name": userName, "location": globalVar.getUser(userName).fence});
-      } else {
-         var foundAdress = null;
-//         geocoder.reverseGeocode( globalVar.getUser(userName).lat, globalVar.getUser(userName).lon, function ( err, data ) {
-            // do something with data
-//            logmodule.writelog(data.results[0].formatted_address);
-//            foundAdress = data.results[0].formatted_address;
-         getLocationAdress(userName, function(foundAdress) {
-            logmodule.writelog("getLocationAdress in getString enter");
-            if (foundAdress !== null && foundAdress !== false) {
-               logmodule.writelog("geoAdress gevonden: " + foundAdress);
-               locationString = __("location_known", {"name": userName, "location": foundAdress});
-            } else {
-               if (foundAdress !== false) {
+   // We need to use a Promise because when the user is not within a known geoFence,
+   // we are going to find the address based on coordinates. And this is an asynchronous
+   // call and as such we need to wait till this is finished
+   return new Promise(function (fulfill, reject){
+      var locationString = '';
+      logmodule.writelog("Create LocationString for user: " + userName);
+
+      // First lets see if the user is in a kown geoFence before we do an expensive trip to
+      // the outside world. We also check if the user is not null. If the user is null, that
+      // means that we have not found a user.
+      if ( userName !== null && globalVar.getUser(userName).fence !== null && globalVar.getUser(userName).fence !== undefined ) {
+         if ( globalVar.getUser(userName).fence !== "" ) {
+            locationString = __("location_known", {"name": userName, "location": globalVar.getUser(userName).fence});
+            // We have found a user and the user is inside a known geoFence, so fulfill te request
+            fulfill(locationString);
+         } else {
+            // We have a user, but the user is not inside a known geoFence, so we are
+            // going to see if we can find an address based on the known coordinates of 
+            // the user.
+            return getLocationAdress(userName).then(function (foundAddress) {
+               logmodule.writelog("getLocationAdress in getString enter");
+               if (foundAddress !== null) {
+                  // We hebben een address ontvangen the async call
+                  logmodule.writelog("foundAddress !== null");
+                  logmodule.writelog("geoAdress gevonden: " + foundAddress);
+                  locationString = __("location_known", {"name": userName, "location": foundAddress});
+                  fulfill(locationString);
+               } else {
+                  // We did not get an address from the async call
                   locationString = __("location_unkown", {"name": userName});
+                  fulfill(locationString);
                }
-            }
-            logmodule.writelog("getLocationAdress in getString quit");
-         });
-      }
-      logmodule.writelog("Locaton string generated: " +locationString);
-   } else {
-      if (userName !== null) {
-         locationString = __("location_nodata", {"name": userName});
+               logmodule.writelog("getLocationAdress in getString quit");
+            });
+         }
       } else {
-         locationString= __("location_nouser");
-      }
+         if (userName !== null) {
+            // This is a fallback when there is a user found, but no data is initialized\
+            // This should not really happen
+            locationString = __("location_nodata", {"name": userName});
+            fulfill(locationString);
+         } else {
+            // In this case no user has been found. Most likely due to the fact that the
+            // the name was not recognized by the speech recognition.
+            locationString= __("location_nouser");
+            fulfill(locationString);
+         }
+      };
       logmodule.writelog("Locaton string generated: " + locationString);
-   }
-   return locationString;
+   });
 }
 
-
-function getLocationAdress(userName, callback) {
-   logmodule.writelog("getLocationAdress enter" );
-   var getAdress = null;
-   geocoder.reverseGeocode( globalVar.getUser(userName).lat, globalVar.getUser(userName).lon, function ( err, data ) {
-      // do something with data
-      logmodule.writelog(data.results[0].formatted_address);
-      getAdress = data.results[0].formatted_address;
-      callback(getAdress);
+function getLocationAdress(userName) {
+   // Here we make the call to the reverse geo lookup engine.
+   // Also just to be sure, we use a Promise to try to get the response before the 
+   // speech command is given.
+   return new Promise(function (fulfill, reject){
+      var getAddress = null;
+      logmodule.writelog("getLocationAdress promise enter" );
+      geocoder.reverseGeocode( globalVar.getUser(userName).lat, globalVar.getUser(userName).lon, function ( err, data ) {
+         // do something with data
+         logmodule.writelog("Address data retreived: " + data.results[0].formatted_address);
+         getAddress = data.results[0].formatted_address;
+         fulfill(getAddress);
+      });
+      logmodule.writelog("getLocationAdress promise quit" );
    });
-   callback(false);
    logmodule.writelog("getLocationAdress quit");
 }
 
 function homeySayLocation(args) {
-   Homey.manager('speech-output').say( getLocationString(args.user), function (err, result ) {
-      logmodule.writelog(err);
-      logmodule.writelog(result);
+   return getLocationString(foundUser.userName).then(function(speechline) {
+      logmodule.writelog("Speech output: " + speechline);
+      Homey.manager('speech-output').say( getLocationString(args.user), function (err, result ) {
+         logmodule.writelog(err);
+         logmodule.writelog(result);
+      });
    });
 }
 
