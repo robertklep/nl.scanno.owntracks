d191bb2a13f8013149ec4552aa2f3b385a00db0b
Menno van Grinsven <scanno71@gmail.com>
2017-08-22 17:51:13 +0200

    SDK2: Convert broker logic into a class

--- broker.js
+++ broker.js
@@ -1,149 +1,159 @@
-const Homey     = require('homey');
-var mqtt      = require("mqtt/node_modules/mqtt");
-var globalVar = require("./global.js");
-var logmodule = require("./logmodule.js");
-var handleMessage = require("./messagehandling.js");
+"use strict";
 
-var connectedClient = null;
-var reconnectClient = false;
+const handleOwntracks = require("./messagehandling.js");
 
-module.exports = {
-   connectToBroker: function(args, state) {
-      connectToBroker(args, state);
-   },
-   subscribeToTopic: function(topicName) {
-      subscribeToTopic(topicName);
-   },
-   sendMessageToTopic: function(args) {
-      sendMessageToTopic(args);
-   },
-   getConnectedClient: function() {
-      return connectedClient;
-   },
-   clearConnectedClient: function() {
-      connectedClient = null;
+class brokerOwntracks {
+   constructor(app) {
+      this.Homey     = require('homey');
+      this.mqtt      = require("mqtt/node_modules/mqtt");
+      this.globalVar = app.globalVar;
+      this.logmodule = app.logmodule;
+      this.handleMessage = new handleOwntracks(app);
+
+      this.connectedClient = null;
+      this.reconnectClient = false;
+
+      this.OnInit();
    }
-}
 
-
-function getBrokerURL() {
-   var urlBroker = []
-   if (Homey.ManagerSettings.get('tls') == true) {
-      urlBroker.push("mqtts://");
-   } else {
-      urlBroker.push("mqtt://");
-   };
-   urlBroker.push(Homey.ManagerSettings.get('url'));
-   urlBroker.push(":"+Homey.ManagerSettings.get('ip_port'));
-   logmodule.writelog('info', "Broker URL: "+ urlBroker.join(''));
-   return urlBroker.join('');
-}
-
-function getConnectOptions() {
-   var rejectUnauth = "true";
-   if ( Homey.ManagerSettings.get('selfsigned') == true) {
-      rejectUnauth = "false";
+   OnInit() {
+      this.connectToBroker();
    }
-   var connect_options = {
-      keepalive: 10,
-      username: Homey.ManagerSettings.get('user'),
-      password: Homey.ManagerSettings.get('password'),
-      rejectUnauthorized: rejectUnauth
-   };
-   logmodule.writelog('info', "rejectUnauthorized: " + connect_options.rejectUnauthorized);
-   return connect_options;
-}
-
-function connectToBroker(args, state) {
-//   if (Homey.manager('settings').get('usebroker') == true) {
-   if (Homey.ManagerSettings.get('usebroker') == true) {
-      if (connectedClient == null) {
-         logmodule.writelog('info', "connectedClient == null");
-         try {
-            connectedClient = mqtt.connect(getBrokerURL(), getConnectOptions());
-         } catch(err) {
-            logmodule.writelog('error', "connectToBroker: " +err);
-         }
-
-         connectedClient.on('reconnect', function() {
-            logmodule.writelog('info', "MQTT Reconnect");
-            reconnectClient = true;
-          });
-
-         connectedClient.on('close', function() {
-            logmodule.writelog('info', "MQTT Closed");
-            reconnectClient = true;
-          });
-
-         connectedClient.on('offline', function() {
-            logmodule.writelog('info', "MQTT Offline");
-            reconnectClient = true;
-          });
-
-         connectedClient.on('error', function(error) {
-            logmodule.writelog('error', "MQTT error occured: " + error);
-         });
-
-         // On connection ...
-         connectedClient.on('connect', function (connack) {
-            logmodule.writelog('info', "MQTT client connected");
-            logmodule.writelog('info', "Connected Topics: " + globalVar.getTopicArray());
-            logmodule.writelog('info', "reconnectedClient " + reconnectClient);
-         });
-
-         connectedClient.on('message',function(topic, message, packet) {
-            // When a message is received, call receiveMessage for further processing
-            logmodule.writelog('info', "OnMessage called");
-            handleMessage.receiveMessage(topic, message, args, state);
-         });
-         // Since we are connecting here, we might as well subscribe to the generic
-         // Owmntracks topic.
-         // Since we subscribe to the generic owntracks topic, we do not have to subscribe to
-         // the individual topics. We will get them anyway.
-         subscribeToTopic("owntracks/#");
-         try {
-            connectedClient.subscribe("owntracks/#")
-            logmodule.writelog('info', "Subscribed to owntracks/#" );
-         } catch(err) {
-            logmodule.writelog('error', "connectToBroker: " +err);
-         }
+   
+   getBrokerURL() {
+      var urlBroker = []
+      if (this.Homey.ManagerSettings.get('tls') == true) {
+         urlBroker.push("mqtts://");
+      } else {
+         urlBroker.push("mqtt://");
       };
+      urlBroker.push(this.Homey.ManagerSettings.get('url'));
+      urlBroker.push(":"+this.Homey.ManagerSettings.get('ip_port'));
+      this.logmodule.writelog('info', "Broker URL: "+ urlBroker.join(''));
+      return urlBroker.join('');
    }
-}
 
-function subscribeToTopic(topicName) {
-   if ( globalVar.getTopicArray().indexOf(topicName) == -1 ) {
-
-      // Fill the array with known topics.... We only fill the
-      // topic array because on connection, wel already subsribe
-      // to the generic owntracks topic
-      globalVar.getTopicArray().push(topicName);
+   getConnectOptions() {
+      var rejectUnauth = "true";
+      if ( this.Homey.ManagerSettings.get('selfsigned') == true) {
+         rejectUnauth = "false";
+      }
+      var connect_options = {
+         keepalive: 10,
+         username: this.Homey.ManagerSettings.get('user'),
+         password: this.Homey.ManagerSettings.get('password'),
+         rejectUnauthorized: rejectUnauth
+      };
+      this.logmodule.writelog('info', "rejectUnauthorized: " + connect_options.rejectUnauthorized);
+      return connect_options;
    }
-}
 
-function sendMessageToTopic(args) {
-   // Check if there is already a connection to the broker
-   if (connectedClient == null) {
-      // There is no connection, so create a connection and send the message
-      try {
-         var client = mqtt.connect(getBrokerURL(), getConnectOptions());
-         client.on('connect', function () {
-            client.publish(args.mqttTopic, args.mqttMessage, function() {
-               logmodule.writelog('info', "send " + args.mqttMessage + " on topic " + args.mqttTopic);
-               client.end();
+   connectToBroker(args, state) {
+      const ref = this;
+      if (ref.Homey.ManagerSettings.get('usebroker') == true) {
+         if (ref.connectedClient == null) {
+            ref.logmodule.writelog('info', "connectedClient == null");
+            try {
+               ref.connectedClient = ref.mqtt.connect(ref.getBrokerURL(), ref.getConnectOptions());
+            } catch(err) {
+               ref.logmodule.writelog('error', "connectToBroker: " +err);
+            }
+
+            ref.connectedClient.on('reconnect', function() {
+               ref.logmodule.writelog('info', "MQTT Reconnect");
+               ref.reconnectClient = true;
+             });
+
+            ref.connectedClient.on('close', function() {
+               ref.logmodule.writelog('info', "MQTT Closed");
+               ref.reconnectClient = true;
+             });
+
+            ref.connectedClient.on('offline', function() {
+               ref.logmodule.writelog('info', "MQTT Offline");
+               ref.reconnectClient = true;
+             });
+
+            ref.connectedClient.on('error', function(error) {
+               ref.logmodule.writelog('error', "MQTT error occured: " + error);
             });
-         });
-      } catch(err) {
-         logmodule.writelog('error', "sendMessageToTopic: " +err);
-      }
-   } else {
-      // There is already a connection, so the message can be send
-      try {
-         connectedClient.publish(args.mqttTopic, args.mqttMessage, function() {
-            logmodule.writelog('info', "send " + args.mqttMessage + " on topic " + args.mqttTopic);
-         });
-      } catch(err) {
-         logmodule.writelog('error', "sendMessageToTopic: " +err);
+
+            // On connection ...
+            ref.connectedClient.on('connect', function (connack) {
+               ref.logmodule.writelog('info', "MQTT client connected");
+               ref.logmodule.writelog('info', "Connected Topics: " + ref.globalVar.getTopicArray());
+               ref.logmodule.writelog('info', "reconnectedClient " + ref.reconnectClient);
+            });
+
+            ref.connectedClient.on('message',function(topic, message, packet) {
+               // When a message is received, call receiveMessage for further processing
+               ref.logmodule.writelog('info', "OnMessage called");
+               ref.handleMessage.receiveMessage(topic, message, args, state);
+            });
+            // Since we are connecting here, we might as well subscribe to the generic
+            // Owmntracks topic.
+            // Since we subscribe to the generic owntracks topic, we do not have to subscribe to
+            // the individual topics. We will get them anyway.
+            ref.subscribeToTopic("owntracks/#");
+            try {
+               ref.connectedClient.subscribe("owntracks/#")
+               ref.logmodule.writelog('info', "Subscribed to owntracks/#" );
+            } catch(err) {
+               ref.logmodule.writelog('error', "connectToBroker: " +err);
+            }
+         };
       }
    }
+
+   subscribeToTopic(topicName) {
+      if ( this.globalVar.getTopicArray().indexOf(topicName) == -1 ) {
+
+         // Fill the array with known topics.... We only fill the
+         // topic array because on connection, wel already subsribe
+         // to the generic owntracks topic
+         this.globalVar.getTopicArray().push(topicName);
+      }
+   }
+
+   sendMessageToTopic(args) {
+      const ref = this;
+      // Check if there is already a connection to the broker
+      if (ref.connectedClient == null) {
+         // There is no connection, so create a connection and send the message
+         try {
+            var client = ref.mqtt.connect(ref.getBrokerURL(), ref.getConnectOptions());
+            client.on('connect', function () {
+               client.publish(args.mqttTopic, args.mqttMessage, function() {
+                  ref.logmodule.writelog('info', "send " + args.mqttMessage + " on topic " + args.mqttTopic);
+                  client.end();
+               });
+            });
+         } catch(err) {
+            ref.logmodule.writelog('error', "sendMessageToTopic: " +err);
+         }
+      } else {
+         // There is already a connection, so the message can be send
+         try {
+            ref.connectedClient.publish(args.mqttTopic, args.mqttMessage, function() {
+               ref.logmodule.writelog('info', "send " + args.mqttMessage + " on topic " + args.mqttTopic);
+            });
+         } catch(err) {
+            ref.logmodule.writelog('error', "sendMessageToTopic: " +err);
+         }
+      }
+   }
+
+   getConnectedClient() {
+      return this.connectedClient;
+   }
+   
+   clearConnectedClient() {
+      this.connectedClient = null;
+   }
+
+   updateRef(app) {
+      this.handleMessage.updateRef(app);
+   }
 }
+
+module.exports = brokerOwntracks;
