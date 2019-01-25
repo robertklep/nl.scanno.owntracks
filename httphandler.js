1f3e7f20cafe262352f0730cca1dee555fcd707b
Menno van Grinsven <scanno71@gmail.com>
2019-01-25 20:45:01 +0100

    Revert "owntracks: initial commit for using websockets for retreiving location events"
    This reverts commit 80a9e0a8cf7f9143765ff82130adbe43ebb32abe.

--- httphandler.js
+++ httphandler.js
@@ -1,7 +1,5 @@
 
 const handleOwntracks = require("./messagehandling.js");
-const Homey = require('homey');
-//const {ManagerCloud} = require('homey');
 
 class httpOwntracks {
 
@@ -9,39 +7,7 @@ class httpOwntracks {
       this.globalVar = app.globalVar;
       this.logmodule = app.logmodule;
       this.handleMessage = new handleOwntracks(app);
-      this.myWebhook = null;
-      this.id = '5c487bd3d25fcd0e0e2b9421';
-      this.secret = '852b3ab49b09a11c898fd1539cd29661'
-      this.cloudId=null;
-      this.data = {id:'abcdef'};
-      this.onInit();
    }
-
-   onInit() {
-     const ref=this;
-     Homey.ManagerCloud.getHomeyId(function(err, cloudId) {
-       ref.cloudId = cloudId;
-       ref.logmodule.writelog('debug', 'Cloud-id: '+ ref.cloudId);
-       ref.data = {id: ref.cloudId};
-       ref.myWebhook = new Homey.CloudWebhook( ref.id, ref.secret, ref.data );
-       ref.myWebhook.on('message', args => {
-         ref.logmodule.writelog('debug','Got a webhook message!');
-         ref.logmodule.writelog('debug','headers:'+ JSON.stringify(args.headers));
-         ref.logmodule.writelog('debug','query:'+ JSON.stringify(args.query));
-         ref.logmodule.writelog('debug','body:'+ JSON.stringify(args.body));
-
-         args.body=ref.handleOwntracksEvents(args);
-       })
-       .register()
-       .then(() => {
-         ref.logmodule.writelog('debug','Webhook registered!');
-       })
-       .catch( ref.error )
-
-     });
-
-   }
-
    /*
       handleOwntracksEvents: Here an incoming http request is handled. The received token is
       looked up. If the token exists, then the request comes from a valid user/owntracks client
@@ -64,10 +30,10 @@ class httpOwntracks {
             var dummyTopic = "owntracks/"+currentUser.userName+"/httpendpoint";
             this.handleMessage.receiveMessage(dummyTopic, JSON.stringify(args.body), null, null);
             this.logmodule.writelog('info', "User "+ currentUser.userName + " authenticated");
-            var result = this.createOwntracksLocationResponse();
-            this.logmodule.writelog('debug', "createOwntracksLocationResponse: "+  JSON.stringify(result));
-            return result;
-//             return true;
+//            var result = this.createOwntracksLocationResponse();
+//            this.logmodule.writelog('debug', "createOwntracksLocationResponse: "+  JSON.stringify(result));
+//            return result;	
+             return true;
           }
        } catch(err) {
           this.logmodule.writelog('error', "Error: " +err);
@@ -78,7 +44,7 @@ class httpOwntracks {
    /*
      createOwntracksLocationResponse: Create an array with location JSON strings. This array
      can be used to send the locations of other persons known on this Homey so their TID's are
-     displayed as friends in the owntracks phone apps, just like when using MQTT.
+     displayed as friends in the owntracks phone apps, just like when using MQTT. 
    */
    createOwntracksLocationResponse() {
       var userLocation = {};
@@ -91,7 +57,7 @@ class httpOwntracks {
             userLocation.lat = this.globalVar.getUserArray()[i].lat;
             userLocation.lon = this.globalVar.getUserArray()[i].lon;
             userLocation.tst = this.globalVar.getUserArray()[i].timestamp;
-
+   
             userLocationArray.push(userLocation);
             userLocation = {};
          }
@@ -100,7 +66,7 @@ class httpOwntracks {
          this.logmodule.writelog('error', "createOwntracksLocationResponse: "+ err);
          return err;
       }
-      return userLocationArray;
+      return this.userLocationArray
    }
 }
 
